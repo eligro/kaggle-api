08f49efef0fa95aa05a3464b5721013092b63c52
eligro <eligro91@gmail.com>
2024-08-28 14:57:23 +0300

    comment out unused code

--- kaggle/__init__.py
+++ kaggle/__init__.py
@@ -19,5 +19,5 @@ from __future__ import absolute_import
 from kaggle.api.kaggle_api_extended import KaggleApi
 from kaggle.api_client import ApiClient
 
-api = KaggleApi(ApiClient())
-api.authenticate()
+#api = KaggleApi(ApiClient())
+#api.authenticate()
